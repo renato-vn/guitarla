10a931275256e5682e2acbbc3ac78ce581a172ec
Renato Vicencio N <renatovicencion@gmail.com>
2024-07-16 16:54:50 -0400

    :sparkles: feat: implement cart functionalities (increase and decrease quantity, remove item, clear cart) and add cart persistance

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,93 @@
-import { useState } from "react"
-import Guitar from "./components/Guitar"
-import Header from "./components/Header"
-import { db } from "./data/db"
+import { useState } from "react";
+import Guitar from "./components/Guitar";
+import Header from "./components/Header";
+import { db } from "./data/db";
+import { useEffect } from "react";
 const App = () => {
 
-  const [data, setData] = useState(db)
-  const [cart, setCart] = useState([])
-
-  const addToCart = (item) => {
-    const itemExists = cart.findIndex((cart) => cart.id === item.id)
-    if (itemExists >= 0) {
-      const updatedCart = [...cart];
-      updatedCart[itemExists].quantity++
-      setCart(updatedCart)
-    } else {
-      const itemClone = Object.assign({}, item);
-      itemClone.quantity = 1
-      setCart([...cart, itemClone])
-    }
+  const initialCart = () => {
+    const localStorageCart = localStorage.getItem('cart');
+    return localStorageCart ? JSON.parse(localStorageCart) : []
   }
 
-  console.log({cart})
+  const [data] = useState(db);
+  const [cart, setCart] = useState(initialCart);
+
+  const MIN_ITEMS = 1;
+  const MAX_ITEMS = 5;
+
+  const addToCart = (item) => {
+    const itemExists = cart.findIndex((cart) => cart.id === item.id);
+    if (itemExists >= 0) {
+      if (cart[itemExists].quantity >= MAX_ITEMS) return;
+      const updatedCart = [...cart];
+      updatedCart[itemExists].quantity++;
+      setCart(updatedCart);
+    } else {
+      const itemClone = Object.assign({}, item);
+      itemClone.quantity = 1;
+      setCart([...cart, itemClone]);
+    }
+  };
+
+  const removeFromCart = (id) => {
+    setCart((prevCart) => prevCart.filter((item) => item.id !== id));
+  };
+
+  const increaseQuantity = (id) => {
+    const updatedCart = cart.map((item) =>
+      item.id === id && item.quantity < MAX_ITEMS
+        ? { ...item, quantity: item.quantity + 1 }
+        : item
+    );
+    setCart(updatedCart);
+  };
+
+  const decreaseQuantity = (id) => {
+    const updatedCart = cart.map((item) =>
+      item.id === id && item.quantity > MIN_ITEMS
+        ? { ...item, quantity: item.quantity - 1 }
+        : item
+    );
+    setCart(updatedCart);
+  };
+
+  const clearCart = () => {
+    setCart([]);
+  };
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart))
+  }, [cart])
 
   return (
     <>
-
-      <Header />
+      <Header
+        cart={cart}
+        removeFromCart={removeFromCart}
+        increaseQuantity={increaseQuantity}
+        decreaseQuantity={decreaseQuantity}
+        clearCart={clearCart}
+      />
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-          {
-            data.map((guitar) =>
-              <Guitar key={guitar.id} guitar={guitar} addToCart={addToCart} />
-            )
-          }
-
+          {data.map((guitar) => (
+            <Guitar key={guitar.id} guitar={guitar} addToCart={addToCart} />
+          ))}
         </div>
       </main>
 
-
       <footer className="bg-dark mt-5 py-5">
         <div className="container-xl">
-          <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+          <p className="text-white text-center fs-4 mt-4 m-md-0">
+            GuitarLA - Todos los derechos Reservados
+          </p>
         </div>
       </footer>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
